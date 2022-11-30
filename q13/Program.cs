1b18b6d11b7e4f8b8294283fc6bee7405ad29e13
Peter <peterson.montez9@gmail.com>
2022-11-30 14:55:54 -0300

    q13

---
+++ q13/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+double[] array = new double[]
+{
+    8.4, 8.6, 8.4, 7.0, 7.2, 10.0, 7.2, 9.4, 9.8
+};
+Console.WriteLine(mediaEspecial(array));
+
+double mediaEspecial(double[] array)
+{
+    Array.Sort(array);
+    if (array.Count() >=4)
+    {
+        double soma = array[0] + array[1] + array[array.Count()-1] + array[array.Count()-2];
+        return (float)(soma/4.0);
+    }
+    else
+    {
+        double soma = 0;
+        foreach (var item in array)
+        {
+            soma += item;
+        }
+        return (float)(soma/(float)array.Count());
+    }
+}
